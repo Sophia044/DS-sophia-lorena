a079189b9816e16ebcef3e35b3ab0b84528e4846
Sophia Lorena <batatafrita03177@gmail.com>
2025-05-05 09:18:53 -0300

    só fiz uma atividade de vetor esṕera um pouco

---
+++ exerciciosVetor/src/exerciciosvetor/ExerciciosVetor.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exerciciosvetor;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class ExerciciosVetor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n[] = {3,1,7,23,19,2,10,8,4,14};
+        
+        for (int c=0; c<=9; c++){
+            System.out.print(n [c] + ",");
+        }
+    }
+    
+}
